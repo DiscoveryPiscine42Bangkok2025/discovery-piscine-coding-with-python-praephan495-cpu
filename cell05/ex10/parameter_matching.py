df14a05f7c7ef2a12b08aa4efa1969d71d02f645
praephan495-cpu <praephan495@gmail.com>
2025-09-18 21:48:04 +0000

    cell05-ex10

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,10 @@
+import sys 
+if len(sys.argv)!=2:
+    print('none')
+else:
+    print=sys.argv[1]
+    user_input =input("What was the parameter?")
+    if user_input == param:
+        print("Good jop!")
+    else:
+        print("Nope,sorry...")
